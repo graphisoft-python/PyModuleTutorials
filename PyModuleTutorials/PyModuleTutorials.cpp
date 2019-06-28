c0f85b7d56f40dbb2f9975204399d8e74be44785
WangJun <445212619@qqcom>
2019-06-28 11:03:54 +0800

    first commit

---
+++ PyModuleTutorials/PyModuleTutorials.cpp
@@ -0,0 +1,20 @@
+﻿// PyModuleTutorials.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+
+
+#include<pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(PyModuleTutorials, m) {
+
+	m.doc() = "pybind11 example module";
+
+	// Add bindings here
+	m.def("foo", []() {
+		return "Hello, World! PyModuleTutorials";
+	});
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PyModuleTutorials
+
+## Help Us
+
+* I hope someone can participate in the graphisoft-python project. Help us to write documents and code.
